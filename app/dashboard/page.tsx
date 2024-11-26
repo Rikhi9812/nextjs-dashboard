dac4db5e105fe96a15d12d8262f01ac422a92f3c
Rikhi9812 <rishi.rana606@gmail.com>
2024-11-26 21:58:54 +0900

    add latestInvoices

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,13 +2,13 @@ import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
-import { fetchLatestInvoices, fetchCardData } from "../lib/data";
+import { fetchCardData } from "../lib/data";
 import { Suspense } from "react";
-import { RevenueChartSkeleton } from "../ui/skeletons";
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from "../ui/skeletons";
 
 export default async function Page() {
   // const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
+  // const latestInvoices = await fetchLatestInvoices();
   const {
     totalPaidInvoices,
     totalPendingInvoices,
@@ -35,7 +35,10 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
