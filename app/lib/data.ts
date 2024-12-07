c6d52947698cdf083ecbf163e81bb87ee994988d
Rikhi9812 <rishi.rana606@gmail.com>
2024-12-07 22:40:43 +0900

    add search buuton

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -87,41 +87,50 @@ export async function fetchCardData() {
   }
 }
 
-// const ITEMS_PER_PAGE = 6;
-// export async function fetchFilteredInvoices(
-//   query: string,
-//   currentPage: number,
-// ) {
-//   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
+const ITEMS_PER_PAGE = 6;
+export async function fetchFilteredInvoices(
+  query: string,
+  currentPage: number,
+) {
+  const offset = (currentPage - 1) * ITEMS_PER_PAGE;
+  debugger;
 
-//   try {
-//     const invoices = await sql<InvoicesTable>`
-//       SELECT
-//         invoices.id,
-//         invoices.amount,
-//         invoices.date,
-//         invoices.status,
-//         customers.name,
-//         customers.email,
-//         customers.image_url
-//       FROM invoices
-//       JOIN customers ON invoices.customer_id = customers.id
-//       WHERE
-//         customers.name ILIKE ${`%${query}%`} OR
-//         customers.email ILIKE ${`%${query}%`} OR
-//         invoices.amount::text ILIKE ${`%${query}%`} OR
-//         invoices.date::text ILIKE ${`%${query}%`} OR
-//         invoices.status ILIKE ${`%${query}%`}
-//       ORDER BY invoices.date DESC
-//       LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
-//     `;
+  try {
+    const invoices = await connectionPool.query(`
+      SELECT
+        invoices.id,
+        invoices.amount,
+        invoices.date,
+        invoices.status,
+        customers.name,
+        customers.email,
+        customers.image_url
+      FROM invoices
+      JOIN customers ON invoices.customer_id = customers.id
+      where
+      customers.name ILIKE $1
+      OR customers.email ILIKE $2
+      OR invoices.amount::text ILIKE $3
+      OR invoices.date::text ILIKE $4
+      OR invoices.status ILIKE $5
+    ORDER BY invoices.date DESC
+    LIMIT $6 OFFSET $7
+  `, [
+    `%${query}%`, // $1
+    `%${query}%`, // $2
+    `%${query}%`, // $3
+    `%${query}%`, // $4
+    `%${query}%`, // $5
+    ITEMS_PER_PAGE, // $6
+    offset // $7
+  ]);
 
-//     return invoices.rows;
-//   } catch (error) {
-//     console.error('Database Error:', error);
-//     throw new Error('Failed to fetch invoices.');
-//   }
-// }
+    return invoices.rows;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch invoices.');
+  }
+}
 
 // export async function fetchInvoicesPages(query: string) {
 //   try {
