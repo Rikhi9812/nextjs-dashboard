ded5b3508fc9e79556280fb0ddf63e91f2f46813
Rikhi9812 <rishi.rana606@gmail.com>
2025-01-19 19:42:26 +0900

    add search

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -178,23 +178,23 @@ export async function fetchFilteredInvoices(
 //   }
 // }
 
-// export async function fetchCustomers() {
-//   try {
-//     const data = await sql<CustomerField>`
-//       SELECT
-//         id,
-//         name
-//       FROM customers
-//       ORDER BY name ASC
-//     `;
+export async function fetchCustomers() {
+  try {
+    const data = await connectionPool.query(`
+      SELECT
+        id,
+        name
+      FROM customers
+      ORDER BY name ASC
+    `);
 
-//     const customers = data.rows;
-//     return customers;
-//   } catch (err) {
-//     console.error('Database Error:', err);
-//     throw new Error('Failed to fetch all customers.');
-//   }
-// }
+    const customers = data.rows;
+    return customers;
+  } catch (err) {
+    console.error('Database Error:', err);
+    throw new Error('Failed to fetch all customers.');
+  }
+}
 
 // export async function fetchFilteredCustomers(query: string) {
 //   try {
